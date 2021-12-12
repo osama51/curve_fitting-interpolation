e35b01d9f9bf8b0f21225eabac6dc004ba0f7580
osama51 <m.osos2011@gmail.com>
2021-12-12 21:26:56 +0200

    BREAKTHOUGH

--- curve_fitting.py
+++ curve_fitting.py
@@ -33,11 +33,12 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.pen=pg.mkPen(color='c')
         # self.x=[2.3,3.5,4.6,5.0,6.1,7.0,7.2,8.5,8.6,8.8,8.9, 9, 9.1, 9.6, 10., 10.5]
         # self.signal=[1.2,2.6,3.1,4.2,5.3,5.8,6.4,7.0,7.6,8.0,8.5, 8.7, 9, 9.5, 10.1, 10.6]
-        # self.length_original_data = len(self.signal)
-        self.actionSamples.triggered.connect(self.show_sample_alone)
+        
+        self.actionSamples.triggered.connect(self.show_samples)
         self.actionPlot.triggered.connect(self.draw_signal)
         self.pushButton.pressed.connect(self.error)
         self.actionOpen.triggered.connect(self.prepare_data)
+        self.actionstring.triggered.connect(self.controlling_error_axes)
 
         self.portion_horizontalSlider.valueChanged.connect(self.handle_sliders)
         self.order_horizontalSlider.valueChanged.connect(self.handle_sliders)
@@ -95,10 +96,10 @@ class MainApp(QMainWindow, FORM_CLASS):
         qpixmap = QtGui.QPixmap(qimage)
         self.label.setPixmap(qpixmap)
             
-    def show_sample_alone(self):
+    def show_samples(self):
         if self.actionSamples.isChecked():
-            self.graphicsView.plotItem.clearPlots()
-            self.graphicsView.plot(self.x, self.signal,
+            
+            self.graphicsView.plot(self.x_axis, self.y_axis,
                   pen=None,
                   name="BEP",
                   symbol='x',
@@ -106,17 +107,39 @@ class MainApp(QMainWindow, FORM_CLASS):
                   symbolBrush=pg.mkBrush(255, 255, 0, 255),
                   symbolSize=7)
         else :
-            self.graphicsView.plotItem.clearPlots()
+            if not self.actionPlot.isChecked():
+                self.graphicsView.plotItem.clearPlots()
+   
+    def controlling_error_axes(self):
+        # self.Error_map = np.zeros((len(chunks),len(overlaps),len(orders)))
+        condition1 = self.order_x_radioButton.isChecked() and self.order_y_radioButton.isChecked()
+        condition2 = self.chunks_x_radioButton.isChecked() and self.chunks_y_radioButton.isChecked()
+        condition3 = self.overlap_x_radioButton.isChecked() and self.overlap_y_radioButton.isChecked()
+        if (condition1) or (condition2) or (condition3):
+            self.label_alert.setText("Pick different variables for the same axis!😑")
+        else:
+            self.label_alert.setText("😘😘")
+            
+        if self.order_x_radioButton.isChecked() and self.chunks_y_radioButton.isChecked():
+            self.img_map=self.Error_map[:,1,:]
+        elif self.order_x_radioButton.isChecked() and self.overlap_y_radioButton.isChecked():
+            self.img_map=self.Error_map[1,:,:]
+        elif self.chunks_x_radioButton.isChecked() and self.order_y_radioButton.isChecked():
+            self.img_map=np.transpose(self.Error_map[:,1,:])
+        elif self.chunks_x_radioButton.isChecked() and self.overlap_y_radioButton.isChecked():
+            self.img_map=self.Error_map[:,:,1]
+        elif self.overlap_x_radioButton.isChecked() and self.order_y_radioButton.isChecked():
+            self.img_map=np.transpose(self.Error_map[1,:,:])
+        elif self.overlap_x_radioButton.isChecked() and self.chunks_y_radioButton.isChecked():
+            self.img_map=np.transpose(self.Error_map[:,:,1])
             
     def devide_to_chunks(self,length_of_data, number_of_chunks, overlap_percentage):
         overlap_percentage /= 100
         self.indices_list = [0]
-        ideal_length_of_chunk = ceil(length_of_data/number_of_chunks)
         length_of_chunk = ceil(length_of_data/(number_of_chunks-((number_of_chunks-1)*(overlap_percentage))))
         
         for index in range(0,2*(number_of_chunks-1),2):
-            # normal_index=int(self.indices_list[index])+length_of_chunk
-            # margin=ceil((overlap_percentage/2)*ideal_length_of_chunk)
+
             index_after=self.indices_list[index]+length_of_chunk
             index_before=ceil(index_after-(overlap_percentage*length_of_chunk))
             
@@ -125,18 +148,150 @@ class MainApp(QMainWindow, FORM_CLASS):
                 self.indices_list.append(index_before)
         self.indices_list.append(length_of_data-1)
         
+    def interpolation(self, indices_list, degree):
+        print(indices_list, 'indices', self.indices_list, 'selfindie')
+        first_ind = indices_list[0]
+        self.index1 = self.indices_list[first_ind]
+        self.index2 = self.indices_list[indices_list[1]]+1
+        y=np.array(self.y_axis[self.index1:self.index2])
+        x=np.array(self.x_axis[self.indices_list[indices_list[0]]:self.indices_list[indices_list[1]]+1])
+        self.interpolation_builtin(x, y, degree)
 
-
+    def interpolation_builtin(self, x_data, y_data, degree):
+        coeff = np.polyfit(x_data, y_data,degree)
+        self.y_fitted = np.polyval(coeff, x_data)
+        error = (self.y_fitted - y_data)/y_data
+        error = np.abs(error)
+        error = sum(error[1:])/len(error)
+        return error
+    
+    def error_map(self):
+        chunks= np.arange(1,21)
+        overlaps = np.arange(0,25, 6)
+        orders = np.arange(1,50)
+        progressbar_index = 0
+        progressbar_percentage = 0
+        total_percentage = len(chunks)*len(overlaps)*len(orders)
+        # indices_list = 
+        btengan = 0
+        summ = 0
+        self.Error_map = np.zeros((len(chunks),len(overlaps),len(orders)))
+        for chunk in range(len(chunks)):
+            for overlap in range(len(overlaps)-1):
+                self.devide_to_chunks(len(y_axis),chunks[chunk],overlaps[overlap])
+        
+                for index in range(0,len(self.indices_list),2):
+                    for order in orders:
+                        if btengan == chunks[chunk]:
+                            btengan = 0
+                            self.Error_map[chunk,overlap,order-3] = summ/chunks[chunk]
+                        else:
+                            summ += interpolation(self.x_axis[self.indices_list[index]:self.indices_list[index+1]],
+                                                  self.y_axis[self.indices_list[index]:self.indices_list[index+1]],order)
+                            btengan += 1
+                        progressbar_index += 1
+                        self.Error_map_progressBar.setValue(int((progressbar_index/total_percentage)*100))
+                        
+        plt.imshow(self.Error_map[1,:,:], aspect='auto',
+                   extent =[orders.min(), orders.max(), overlaps.min(), overlaps.max()],
+                   cmap='PuRd', interpolation= 'bessel')
+        plt.show()
+    
+    def sliders_values(self):
+        self.degree = self.order_horizontalSlider.value()
+        self.overlap = self.overlap_horizontalSlider.value()
+        self.chunks = self.chunks_horizontalSlider.value()
         
     def draw_signal(self):
-        self.graphicsView.plot(self.x, self.signal, pen = self.pen, title="Three plot curves")
-        self.devide_to_chunks(1000,5,20)
-        self.interpolation([0,1],500)
+        self.graphicsView.plot(self.x_axis, self.y_axis, pen = self.pen, title="Three plot curves")
+        self.sliders_values()
+        self.devide_to_chunks(len(self.y_axis),self.chunks,self.overlap)
+        # self.interpolation(self.x_axis, self.y_axis ,self.degree)
         self.draw_interpolation()
-        self.mathTex_to_QPixmap()
+        # self.mathTex_to_QPixmap()
         if self.actionSamples.isChecked():
-            self.self.error()
+            self.show_samples()
 
+    def draw_interpolation(self):
+        x_axis = np.linspace(0, 1000,1000)
+
+        y_int = np.zeros(len(self.y_axis))
+        i = 1
+        for chunk in range(0,self.chunks,1):
+            self.interpolation([chunk+i-1,chunk+i],self.degree)
+            y_int[self.index1:self.index2] += self.y_fitted
+            i += 1
+        # m = 0
+        # for h in range(1,chunks,1):
+        #     y_int[self.indices_list[2*h]:self.indices_list[2*h-1]+1] /= 2
+        #     print(self.indices_list[2*h],self.indices_list[2*h-1])
+        self.graphicsView.plot(x_axis, y_int, pen ='g')
+    
+    def error(self):
+        if not self.flag:
+            self.pushButton.setText('Cancel')
+            self.flag = 1
+        else:
+            self.pushButton.setText('Start')
+            self.flag = 0
+            
+    def portion(self,data_lenght,portion_percentage):
+        self.indices_list =[0]
+        last_index = int(((portion_percentage/100)*data_lenght)-1)
+        self.indices_list.append(last_index)
+
+    #_____________________________FETCHING DATA________________________#
+    
+    def prepare_data(self):
+        self.browse()
+        self.read_data()
+        self.y_axis = self.amplitude
+        self.x_axis = self.timestamps
+
+        
+    def browse(self):
+        self.file_path_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', " ", "(*.txt *.csv *.xls)")
+
+    def read_data(self):
+        self.file_name, self.file_extension = os.path.splitext(self.file_path_name)
+        
+        if self.file_extension == '.txt':
+            # to skip the second row, use a list-like argument "[]"
+            # fwf stands for fixed width formatted lines.
+            data = pd.read_fwf(self.file_path_name)
+            self.signal_name = data.columns[1]
+            self.amplitude = data.iloc[:, 1] # gets second column
+            # : means everything in dimension1 from the beginning to the end
+            self.timestamps = data.iloc[:, 0]
+            self.interval = self.timestamps[6]- self.timestamps[5]
+            
+        elif self.file_extension == '.csv':
+            data = pd.read_csv(self.file_path_name)
+            self.signal_name = data.columns[1]
+            self.amplitude = data.iloc[:, 1] # gets second column
+            self.timestamps = data.iloc[:, 0]
+            self.interval = self.timestamps[6]- self.timestamps[5]
+        self.sampling_freq = 1000#1/interval
+        self.length_original_data = len(self.amplitude)
+        # xy_axes = {'xaxis': xaxis_timestamps, 'yaxis': yaxis_values}
+        # return xy_axes;
+
+    
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+if __name__ == '__main__':
+    main()
+    
+    # sp.preview(expr, euler = False, preamble = r"\documentclass{standalone}"
+    #                    r"\usepackage{pagecolor}"
+    #                    r"\definecolor{graybg}{HTML}" + the_color +
+    #                    r"\pagecolor{graybg}"
+    #                    r"\begin{document}",
+    #                    viewer = "BytesIO", output = "ps", outputbuffer=f)
+'''
     def error(self):
         if not self.flag:
             self.pushButton.setText('Cancel')
@@ -249,63 +404,4 @@ class MainApp(QMainWindow, FORM_CLASS):
         result = map(lambda original_data, interpolated_data: abs((interpolated_data - original_data)/original_data), y, yint_picked)
         result = list(result)
         error = sum(result[1:-1])/len(result[1:-1])
-        return error 
-    
-    def draw_interpolation(self):
-        self.graphicsView.plot(self.xx, self.yint, pen ='g')
-        
-    def portion(self,data_lenght,portion_percentage):
-        self.indices_list =[0]
-        last_index = int(((portion_percentage/100)*data_lenght)-1)
-        self.indices_list.append(last_index)
-
-    #_____________________________FETCHING DATA________________________#
-    
-    def prepare_data(self):
-        self.browse()
-        self.read_data()
-        self.signal = self.amplitude
-        self.x = self.timestamps
-        print(len(self.x))
-        
-    def browse(self):
-        self.file_path_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', " ", "(*.txt *.csv *.xls)")
-
-    def read_data(self):
-        self.file_name, self.file_extension = os.path.splitext(self.file_path_name)
-        
-        if self.file_extension == '.txt':
-            # to skip the second row, use a list-like argument "[]"
-            # fwf stands for fixed width formatted lines.
-            data = pd.read_fwf(self.file_path_name)
-            self.signal_name = data.columns[1]
-            self.amplitude = data.iloc[:, 1] # gets second column
-            # : means everything in dimension1 from the beginning to the end
-            self.timestamps = data.iloc[:, 0]
-            self.interval = self.timestamps[6]- self.timestamps[5]
-            
-        elif self.file_extension == '.csv':
-            data = pd.read_csv(self.file_path_name)
-            self.signal_name = data.columns[1]
-            self.amplitude = data.iloc[:, 1] # gets second column
-            self.timestamps = data.iloc[:, 0]
-            self.interval = self.timestamps[6]- self.timestamps[5]
-        self.sampling_freq = 1000#1/interval
-        # xy_axes = {'xaxis': xaxis_timestamps, 'yaxis': yaxis_values}
-        # return xy_axes;
-
-    
-def main():
-    app = QApplication(sys.argv)
-    window = MainApp()
-    window.show()
-    app.exec_()
-if __name__ == '__main__':
-    main()
-    
-    # sp.preview(expr, euler = False, preamble = r"\documentclass{standalone}"
-    #                    r"\usepackage{pagecolor}"
-    #                    r"\definecolor{graybg}{HTML}" + the_color +
-    #                    r"\pagecolor{graybg}"
-    #                    r"\begin{document}",
-    #                    viewer = "BytesIO", output = "ps", outputbuffer=f)
+        return error '''
